57cb4aace7c58ba71115b98edcc08c290b754f32
Daniel Collin <daniel@collin.com>
2011-10-25 17:08:59 +0200

    Now generating a proper CFG
    Fully scraped the old algorithm and wrote a new simpler one that generates a proper CFG
    
    Also added support to write out graphwiz files

--- ssagen.c
+++ ssagen.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -12,6 +13,7 @@ static uint8_t* s_ptr = (uint8_t*)s_data;
 
 #define ALIGNOF(t) __alignof__(t)
 #define alloc(type) (type*)allocAligned(sizeof(type), ALIGNOF(type))
+#define allocZero(type) (type*)allocZeroAligned(sizeof(type), ALIGNOF(type))
 #define allocArray(type, count) (type*)allocAligned(sizeof(type) * count, ALIGNOF(type))
 #define allocZeroArray(type, count) (type*)allocZeroAligned(sizeof(type) * count, ALIGNOF(type))
 #define sizeof_array(array) (int)(sizeof(array) / sizeof(array[0]))
@@ -35,6 +37,13 @@ void* allocAligned(uint32_t size, uint32_t alignment)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void* allocPtr()
+{
+	return s_ptr;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void* allocZeroAligned(uint32_t size, uint32_t alignment)
 {
 	void* data = allocAligned(size, alignment);
@@ -50,6 +59,9 @@ typedef struct Instruction
 	const char* regName0;
 	const char* regName1;
 	const char* regName2;
+	bool leader;
+	bool branch;
+	bool condBranch;
 
 } Instruction;
 
@@ -58,10 +70,10 @@ typedef struct BasicBlock
 	uint64_t id;
 
 	Instruction* instructions;
-	struct BasicBlock** blocks;
+	struct BasicBlock* edges[16];		// temporary fixed size but should be fine for testing
 
 	int instructionCount;
-	int basicBlocks;
+	int edgesCount;
 
 	int instructionStart;
 	int instructionEnd;
@@ -76,15 +88,15 @@ typedef struct BasicBlock
 
 static Instruction testCode[] =
 {
-	{ "b", "6" }, // goto B
+	{ "bc", "6", "B" }, // goto B
 	{ "inst", "1" },
 	{ "inst", "2" },
-	{ "b", "6", },
+	{ "bc", "6", "C" },
 	{ "inst", "3" },
 	{ "inst", "4" },
-	{ "b", "-6", },
+	{ "bc", "-6", "A" },
 	{ "inst", "5" },
-	{ "b", "1", },
+	{ "b", "1", "C" },
 	{ "inst", "6" },
 	{ "inst", "7" },
 	{ "inst", "8" },
@@ -107,82 +119,124 @@ inst 8
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-static int sortOffsets(const void* a, const void* b)
-{
-	return (*(int*)a - *(int*)b);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // First build a Control Flow Graph (CFG) of the code
 
 BasicBlock* buildCFG(Instruction* instructions, const int instructionCount)
 {
-	int* offsets = allocArray(int, instructionCount);
-	int offsetCount = 1;
+	// First pass we just mark instructions if they are leaders or not 
 
-	memset(offsets, 0x0f, sizeof(int) * instructionCount); // clear to big value
-	offsets[0] = 0; // the first block is always a basic block
-
-	// First pass we just parse the instruction stream to find labels 
+	instructions[0].leader = true; // Rule 1: First instruction is leader
 
 	for (int i = 0; i < instructionCount; ++i)
 	{
-		if (!strcmp("b", instructions[i].opCode))
+		bool branch = !strcmp("b", instructions[i].opCode); 
+		bool condBranch = !strcmp("bc", instructions[i].opCode); 
+
+		if (branch || condBranch)
 		{
 			int offset = atoi(instructions[i].regName0);
-			offsets[offsetCount + 0] = i + offset; // branch target
-			offsets[offsetCount + 1] = i + 1; // instruction after branch makes up the code as a basic block
-			offsetCount += 2;
-			i++;
+			instructions[i + offset].leader = true;			// Rule 2: Target for branch is leader
+
+			if (i != instructionCount-1)
+				instructions[i + 1].leader = true;			// Rule 3: instruction after branch is leader 
 		}
+
+		// cache branch status
+
+		instructions[i].branch = branch;
+		instructions[i].condBranch = condBranch;
 	}
 
-	// end of the instruction stream is a offset as well
-	offsets[offsetCount++] = instructionCount - 1;
+	// Second pass build basic blocks from leaders
+	
+	int basicBlockCount = 0;
 
-	// sort the offsets
+	int count = 0;
+	BasicBlock* block = 0;
+	BasicBlock* blocks = allocPtr();
 
-	printf("offsets pre-sort\n");
+	for (int i = 0; i < instructionCount; ++i)
+	{
+		if (instructions[i].leader)
+		{
+			if (block)
+			{
+				block->instructionCount = count;
+				block->instructionStart = i - count;
+				block->instructionEnd = i;
+				count = 0;
+			}
 
-	for (int i = 0; i < offsetCount; ++i)
-		printf("%d\n", offsets[i]);
+			block = allocZero(BasicBlock);
+			block->instructions = &instructions[i];
 
-	qsort(offsets, offsetCount, sizeof(int), sortOffsets);
+			block->id = basicBlockCount;
+			basicBlockCount++;
+		}
 
-	printf("offsets sort\n");
+		count++;
+	}
 
-	for (int i = 0; i < offsetCount; ++i)
-		printf("%d\n", offsets[i]);
+	// finish last block if we got one
 
-	// Create the code blocks and assign the instructions
+	if (block)
+	{
+		block->instructionCount = count;
+		block->instructionStart = instructionCount - count;
+		block->instructionEnd = instructionCount;
+	}
 
-	int blockCount = offsetCount - 1;
+	// Pass 3. Link the BasicBlocks togheter
 
-	BasicBlock* blocks = allocArray(BasicBlock, blockCount);
+	for (int b = 0; b < basicBlockCount-1; ++b)
+	{
+		BasicBlock* block = &blocks[b];
+		
+		for (int i = 0, instCount = block->instructionCount; i < instCount; ++i)
+		{
+			if (block->instructions[i].branch || block->instructions[i].condBranch)
+			{
+				int offset = atoi(block->instructions[i].regName0);
+				int instructionOffset = block->instructionStart + i + offset;
 
-	for (int i = 0; i < blockCount; ++i)
+				// find the block to link with
+				
+				for (int f = 0; f < basicBlockCount; ++f)
+				{
+					BasicBlock* tempBlock = &blocks[f];
+
+					// Check if inside range and link if that is the case
+
+					if (instructionOffset >= tempBlock->instructionStart &&
+						instructionOffset < tempBlock->instructionEnd)
+					{
+						block->edges[block->edgesCount++] = tempBlock;
+						break;
+					}
+				}
+			}
+		}
+
+		// if last instruction isn't a unconditonal branch link the block with the next one
+
+		if (!block->instructions[block->instructionCount - 1].branch)
+			block->edges[block->edgesCount++] = &blocks[b + 1];
+	}
+	/*
+	for (int i = 0; i < basicBlockCount; ++i)
 	{
 		BasicBlock* block = &blocks[i];
-		block->id = i;
-		block->instructions = &instructions[offsets[i]];
-		block->instructionCount = offsets[i + 1] - offsets[i]; 
-		block->instructionStart = offsets[i];
-		block->instructionEnd = offsets[i + 1];
 
-		printf("block->id %d\n", (int)block->id);
+		printf("==================================\n");
+		//printf("id %d\n", (int)blocks[i].id);
+		
+		printf("block	                %p\n", block);
+		printf("block->edgesCount       %d\n", block->edgesCount);
+		printf("block->firstEdge        %p\n", block->edges[0]);
 		printf("block->instructions     %p\n", block->instructions);
 		printf("block->instructionCount %d\n", block->instructionCount);
 		printf("block->instructionStart %d\n", block->instructionStart);
 		printf("block->instructionEnd   %d\n", block->instructionEnd);
-	}
-
-	// dump out the blocks
-	printf("blockCount %d\n", blockCount);
-
-	for (int i = 0; i < blockCount; ++i)
-	{
-		printf("id %d\n", (int)blocks[i].id);
 
 		for (int count = 0; count < blocks[i].instructionCount; ++count)
 		{
@@ -200,8 +254,52 @@ BasicBlock* buildCFG(Instruction* instructions, const int instructionCount)
 
 		printf("\n");
 	}
+	*/
 
-	return blocks;
+	// dump out a graphviz file
+
+	FILE* file = fopen("/Users/daniel/test.gv", "wt");
+
+	fprintf(file, "digraph G {\ngraph [\nrankdir = \"LR\"\n];\nnode [\nfontsize = \"16\"\n shape = \"ellipse\"\n];\n edge [\n];\n");
+
+	for (int i = 0; i < basicBlockCount; ++i)
+	{
+		BasicBlock* block = &blocks[i];
+
+		fprintf(file, "\"node%d\" [\nlabel = \"", (int)block->id);
+
+		for (int count = 0; count < block->instructionCount; ++count)
+		{
+			fprintf(file, " %s ", blocks[i].instructions[count].opCode);
+
+			if (blocks[i].instructions[count].regName0)
+				fprintf(file, "%s ", blocks[i].instructions[count].regName0);
+			if (blocks[i].instructions[count].regName1)
+				fprintf(file, "%s ", blocks[i].instructions[count].regName1);
+			if (blocks[i].instructions[count].regName2)
+				fprintf(file, "%s ", blocks[i].instructions[count].regName2);
+
+			fprintf(file, "\\n");
+		}
+
+		fprintf(file, "\"\nshape = \"ellipse\"\n];\n");
+
+		//for (int c = 0; c < block->edgesCount; ++c)
+		//	fprintf(file, "b%d -> b%d;\n", (int)block->id, (int)block->edges[c]->id);
+	}
+
+	for (int i = 0; i < basicBlockCount; ++i)
+	{
+		BasicBlock* block = &blocks[i];
+
+		for (int c = 0; c < block->edgesCount; ++c)
+			fprintf(file, "\"node%d\":f0 -> \"node%d\":f0 [\nid = %d\n];\n", (int)block->id, (int)block->edges[c]->id, (int)block->id);
+	}
+
+	fprintf(file, "}\n");
+	fclose(file);
+
+	return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
